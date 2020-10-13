58eee19ff1e7d3e436918f4fd6927c0745f326a4
GabrielaRamos23 <gabrielaramosch23@gmail.com>
2020-10-13 12:18:02 -0500

    uploading-40

---
+++ RGabriela-40.php
@@ -0,0 +1,9 @@
+<?php
+echo "I feel good";
+print "I feel bad";
+echo "What time is it?";
+print "It is nine o'clock";
+echo "How much does it cost?";
+print "It costs ten dollars";
+echo "What is going on?";
+?>
