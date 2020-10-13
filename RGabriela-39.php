f854c4439eb3fb24a2d06c30af7100196e57256f
GabrielaRamos23 <gabrielaramosch23@gmail.com>
2020-10-13 12:13:25 -0500

    uploading-39

---
+++ RGabriela-39.php
@@ -0,0 +1,14 @@
+<?php
+echo "I like to play soccer";
+print "I am a single";
+echo "I am married";
+print "Do you have children?";
+echo "Me too";
+print "It is late";
+echo "Where are you going?";
+print "I am going shopping";
+echo "I am in a hurry";
+print "That is right";
+echo "As soon as possible";
+print "How is it going?";
+?>
